2a1414b9660bc3ed65957e784a3f3b520c716517
jargyl <jari.gielen@gmail.com>
2023-04-21 22:06:18 +0200

    init

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+
+
+export default function Home() {
+  return (
+    <main className="font-montserrat">
+      <h1 className="text-6xl font-bold">END.</h1>
+    </main>
+  )
+}
