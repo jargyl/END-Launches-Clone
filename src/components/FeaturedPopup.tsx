74dc13cad5ce6a736fbb1a7d7a0a9f94cca5a8f4
jargyl <jari.gielen@gmail.com>
2023-05-08 12:39:15 +0200

    Added custom Features category popup

--- src/components/FeaturedPopup.tsx
+++ src/components/FeaturedPopup.tsx
@@ -1,12 +1,54 @@
 import React, { Component } from "react";
+import features from "../../public/data/features.json";
+
+interface Feature {
+  subcategoryTitle: string;
+  subCategories: string[];
+  subImageCategory: string[];
+  subImageTitles: string[];
+  subImages: string[];
+}
+
+const featuresData: Feature = features;
 
 export default class FeaturedPopup extends Component {
   render() {
     return (
       <div className="w-full max-w-screen-lg mx-auto">
         <div className="flex justify-between h-[29rem] overflow-hidden">
-          <p>test</p>
-          <p>test</p>
+          <div>
+            <p className="font-bold absolute">
+              {featuresData.subcategoryTitle}
+            </p>
+            <div className="flex flex-col">
+              {featuresData.subCategories.map((subCategory, index) => (
+                <div key={index} className="mb-2 relative top-8">
+                  {subCategory}
+                </div>
+              ))}
+            </div>
+          </div>
+          <div className="flex flex-wrap pl-10">
+            {featuresData.subImageCategory.map((category, index) => (
+              <div key={index} className="w-1/3 px-2">
+                <div className="relative">
+                  <img
+                    src={featuresData.subImages[index]}
+                    alt={featuresData.subImageTitles[index]}
+                    className="w-full h-48 object-cover"
+                  />
+                  <div className="absolute bottom-0 w-full bg-black bg-opacity-50 py-2 px-2">
+                    <div className="text-white uppercase text-xs">
+                      {category}
+                    </div>
+                    <div className="text-white font-bold text-xs">
+                      {featuresData.subImageTitles[index]}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     );
--- src/components/LaunchesPopup.tsx
+++ src/components/LaunchesPopup.tsx
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import launches from "../../public/data/launches.json";
+import "simplebar-react/dist/simplebar.min.css";
+import SimpleBarReact from "simplebar-react";
 
 interface Launch {
   model: string;
@@ -14,23 +16,25 @@ export default class LaunchesPopup extends Component {
   render() {
     return (
       <div className="w-full max-w-screen-lg mx-auto">
-        <p className="font-bold">View upcoming releases</p>
-        <div className="flex flex-wrap justify-between overflow-hidden">
-          {launchesData.map((launch, index) => (
-            <div key={index} className="w-1/2 py-2 pr-2 flex">
-              <img
-                src={launch.image}
-                alt={launch.model}
-                className="w-3/5 mr-4"
-              />
-              <div>
-                <p className="pb-1">{launch.model}</p>
-                <p className="text-gray-500 pb-1">{launch.colorway}</p>
-                <p>{launch.release_date}</p>
+        <p className="font-bold absolute">View upcoming releases</p>
+        <SimpleBarReact className="max-h-full h-[29rem] pt-6 absolute">
+          <div className="flex flex-wrap justify-between">
+            {launchesData.map((launch, index) => (
+              <div key={index} className="w-1/2 py-2 pr-2 flex">
+                <img
+                  src={launch.image}
+                  alt={launch.model}
+                  className="w-3/5 mr-4"
+                />
+                <div>
+                  <p className="pb-1">{launch.model}</p>
+                  <p className="text-gray-500 pb-1">{launch.colorway}</p>
+                  <p>{launch.release_date}</p>
+                </div>
               </div>
-            </div>
-          ))}
-        </div>
+            ))}
+          </div>
+        </SimpleBarReact>
       </div>
     );
   }
