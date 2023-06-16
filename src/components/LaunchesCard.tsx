8db7b53d994ee6c5bacaa31bdad0860e98b99c12
jargyl <jari.gielen@gmail.com>
2023-06-16 22:39:51 +0200

    Added more products Finished LaunchesOverview

--- src/components/LaunchesCard.tsx
+++ src/components/LaunchesCard.tsx
@@ -1,25 +1,40 @@
 import React, { Component } from "react";
 import { MdNavigateNext } from "react-icons/md";
 
-export default class LaunchesCard extends Component {
+interface LaunchesCardProps {
+  date: string;
+  brand: string;
+  model: string;
+  imageUrl: string;
+  price: string;
+}
+
+export default class LaunchesCard extends Component<LaunchesCardProps> {
   render() {
+    const { date, brand, model, imageUrl, price } = this.props;
+
+    const parsedDate = new Date(date);
+    const dayNumber = parsedDate.getDate();
+    const month = parsedDate.toLocaleString("default", { month: "long" });
+
     return (
-      <div className="h-[30rem] w-[24rem] bg-gray-100 relative rounded p-5 flex flex-col justify-between hover:shadow-xl transition-shadow duration-200">
+      <div className="h-[30rem] w-[24rem] bg-gray-100 relative rounded m-5 p-5 flex flex-col justify-between hover:shadow-xl transition-shadow duration-200">
         <div className="absolute bg-gray-300 h-12 rounded flex items-center">
           <div className="h-8 w-8 ml-2 bg-white flex items-center justify-center rounded font-[600] text-sm">
-            23
+            {dayNumber}
           </div>
-          <p className="mx-3 text-white uppercase font-[500] text-sm">June</p>
+          <p className="mx-3 text-white uppercase font-[500] text-sm">
+            {month}
+          </p>
         </div>
-        <img
-          src="https://launches-media.endclothing.com/DD1503-103_launches_thumbnailv2_1_1_1.jpg"
-          alt="product"
-        />
+        <div className="flex-grow flex flex-col justify-center">
+          <img src={imageUrl} alt="product" />
+        </div>
         <div className="flex items-center justify-between">
           <div>
-            <p className="uppercase text-sm font-[500] mb-2">Nike</p>
-            <p className="uppercase font-[500] mb-2">Nike Dunk Low W</p>
-            <p className="text-sm">€115</p>
+            <p className="uppercase text-sm font-[500] mb-2">{brand}</p>
+            <p className="uppercase font-[500] mb-2">{model}</p>
+            <p className="text-sm">€{price}</p>
           </div>
           <div>
             <button className="text-white h-10 w-10 flex items-center justify-center rounded-full bg-white">
--- src/components/LaunchesOverview.tsx
+++ src/components/LaunchesOverview.tsx
@@ -1,11 +1,22 @@
 import React, { Component } from "react";
 import LaunchesCard from "./LaunchesCard";
+import launches from "../../public/data/launches.json";
 
 export default class LaunchesOverview extends Component {
   render() {
     return (
-      <div>
-        <LaunchesCard />
+      <div className="w-full max-w-screen-lg mx-auto">
+        <div className="flex justify-center flex-wrap">
+          {launches.map((launch) => (
+            <LaunchesCard
+              date={launch.release_date}
+              brand={launch.brand}
+              model={launch.model}
+              imageUrl={launch.image}
+              price={launch.retail_price}
+            />
+          ))}
+        </div>
       </div>
     );
   }
